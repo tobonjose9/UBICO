1e4a07332dba4d9a72b2028a5224002ecf8a403c
Jose Alejandro Tobon <tobonjose9@gmail.com>
2024-04-28 17:40:40 -0500

    Creacion

---
+++ src/main/java/com/co/ubico/reservas/UbicoApplication.java
@@ -0,0 +1,13 @@
+package com.co.ubico.reservas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UbicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/co/ubico/reservas/UbicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.ubico.reservas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
